1a6329650dc8721b47c8b4e8cc401fbc821da5d9
Nass213dz <nassouben44@gmail.com>
2026-02-03 13:18:41 +0100

    feat(Program.cs): created program.cs file + class

---
+++ Program.cs
@@ -0,0 +1,15 @@
+using easySave_BMT.ViewModel_;
+
+namespace easySave_BMT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            ViewModel viewModel = new ViewModel();
+
+            viewModel.RunApp();
+        }
+    }
+}
