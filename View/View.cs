72465f56ab7cf8dce089e2e30cbf51d45174757d
prnhpci <mjh15179389g@gmail.com>
2026-02-03 12:56:35 +0100

    feat(View.cs): partial view ending

--- View/View.cs
+++ View/View.cs
@@ -6,9 +6,10 @@ namespace easySave_BMT.View_
 {
     public class View
     {
-        //Default ctor
+        
         private ViewModel viewModel;
 
+        //ctor
         public View(ViewModel viewModel)
         {
             this.viewModel = viewModel;
@@ -24,53 +25,12 @@ namespace easySave_BMT.View_
                 "\n2 - Ajouter une sauvegarde" +
                 "\n3 - Faites un backup" +
                 "\n4 - Supprimer une sauvegarde" +
-                "\n5 - Quitter" 
+                "\n" +"\n" +"\n" +
+                "\n5 - Quitter !" 
                 );
-
-                
+            return CheckChoiceMenu(Console.ReadLine(), 1, 5);
         }
-
-        //Verify if input is integer
-        private static bool CheckInt(string input)
-        {
-            try
-            {
-                int.Parse(input);
-                return true;
-            }
-            catch
-            {
-                return false;
-            }
-        }
-
-        //Display all saves
-        private void SavesJobReport(int shift)
-        {
-            var saves = this.viewModel.model.saves;
-
-            for (int i =0; i<saves.count; i++)
-            {
-                Console.WriteLine(
-                    "\n" + (i + shift) + " - " + "Nom: " + saves[i].name
-                    + "\n      Source: " + saves[i].src 
-                    + "\n      Destination: " + saves[i].dst
-                    +"\n       Type: " + saves[i].backupType
-                );
-            }
-        }
-
-        public void DisplayAllSaves()
-        {
-            Console.Clear();
-            Console.WriteLine("liste des sauvegardes : ");
-
-            //Display all saves
-            SavesJobReport(1);
-            DisplayMessage(1);
-        }
-
-
+        
         //Display message on console
         public void DisplayMessage(int id)
         {
@@ -227,6 +187,320 @@ namespace easySave_BMT.View_
                 }
             }
             Console.ResetColor();
+        }
+
+        //Verify if input is integer
+        private static bool CheckInt(string input)
+        {
+            try
+            {
+                int.Parse(input);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        //Add BackupType
+        public int AddSaveBackupType()
+        {
+            Console.WriteLine(
+                "\nChoisir le type de backup: "+
+                "\n1.Complète "+
+                "\n2.Differentielle"
+            );
+            return CheckChoiceMenu(Console.ReadLine(),0,2);
+        }
+
+        //Verify if the name is valid
+
+        private bool CheckName(string name)
+        {
+            int length = name.Length;
+            if(length >= 1 && length <= 20)
+            {
+                if(!this.viewModel.model.saves.Exists(save => save.name == name))
+                {
+                    return true;
+                }
+                DisplayMessage(214);
+                return false;
+            }
+            DisplayMessage(215);
+            return false;
+        }
+
+        //Display all saves
+        private void SavesJobReport(int shift)
+        {
+            var saves = this.viewModel.model.saves;
+
+            for (int i =0; i<saves.count; i++)
+            {
+                Console.WriteLine(
+                    "\n" + (i + shift) + " - " + "Nom: " + saves[i].name
+                    + "\n      Source: " + saves[i].src 
+                    + "\n      Destination: " + saves[i].dst
+                    +"\n       Type: " + saves[i].backupType
+                );
+            }
+        }
+
+        public void DisplayAllSaves()
+        {
+            Console.Clear();
+            Console.WriteLine("liste des sauvegardes : ");
+
+            //Display all saves
+            SavesJobReport(1);
+            DisplayMessage(1);
+        }
+
+        //Add a save name
+        public string SaveName()
+        {
+            Console.Clear();
+            Console.WriteLine("Paramètre de sauvegarde");
+            DisplayMessage(2);
+
+            Console.WriteLine("\nEntrer un nom (1 à 20 caractères):");
+            string name = Console.ReadLine();
+
+            //Verify name
+            while (!CheckName(name))
+            {
+                name = Console.ReadLine();
+            }
+            return name;
+        }
+
+        private string RectifyPath(string path)
+        {
+            if(path != "0" && path.Length >= 1)
+            {
+                path += (path.EndsWith("/") || path.EndsWith("\\")) ? "" : "\\";
+                path = path.Replace("/", "\\");
+            }
+            return path.ToLower();
+        }
+
+        //source
+        public string SaveSrc()
+        {
+            Console.WriteLine("\nEntrez la source du repertoire ");
+            string src = RectifyPath(Console.ReadLine());
+
+            //Verify the src is valid
+            while(
+                !Directory.Exists(src) && src != "000" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "" +
+                "")
+            {
+                DisplayMessage(211);
+                src = RectifyPath(Console.ReadLine());
+            }
+            return src;
+        }
+
+        
+
+        //save destination
+
+        public string ChecksaveDst(string src, string dst)
+        {
+            if(dst == "0")
+            {
+                return true;
+            }
+            else if (Directory.Exists(dst))
+            {
+                if(src != dst)
+                {
+                    if(dst.Length > src.length)
+                    {
+                        if(src != dst.Substring(0, src.Length))
+                        {
+                            return true;
+                        }
+                        else
+                        {
+                            DisplayMessage(217);
+                            return false;
+                        }
+                    }
+                    return true;
+                }
+                DisplayMessage(212);
+                return false;
+            }
+            DisplayMessage(213);
+            return false;
+        }
+        public string SaveDst(string src)
+        {
+            Console.WriteLine("\nEntrer la destination du répertoire.");
+            string dst = RectifyPath(Console.ReadLine());
+
+            //Verfify if the destination is valid
+
+            while (!ChecksaveDst(src, dst))
+            {
+                dst= RectifyPath(Console.ReadLine());
+            }
+            return dst;
+        }
+
+        //Display size
+        private string DisplaySize(long octet)
+        {
+            if(octet > 1000000000000)
+            {
+                return Math.Round((decimal)octet / 1000000000000, 2)+ "To";
+            }else if(octet > 1000000000)
+            {
+                return Math.Round((decimal)octet / 1000000000, 2) + "Go";
+            }else if(octet > 1000000)
+            {
+                return Math.Round((decimal)octet / 1000000, 2)+ "Mo";
+            }
+            else if(octet > 1000)
+            {
+                return Math.Round((decimal)octet / 1000, 2) + "ko";
+            }
+            else
+            {
+                return octet + "o";
+            }
+        }
+
+        private void DisplayProgressBar(int percent)
+        {
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write("Progression: [ " + percent + " %]");
+            Console.ResetColor();
+
+            Console.Write(" [");
+            for (int i = 0; i < 100; i += 5)
+            {
+                if (percent > i)
+                {
+                    Console.Write("#");
+                }
+                else
+                {
+                    Console.Write(".");
+                }
+            }
+            Console.Write("]\n\n");
+        }
+        
+
+
+        public void DisplayCurrentState(string name, int fileLeft, long leftSize, long curSize, int percent)
+        {
+            Console.Clear();
+            Console.WriteLine(
+                "Backup actuelle : " + name
+                +"\nTaille des fichiers: " + DisplaySize(curSize)
+                +"\nNombre de fchiers restants: "+fileLeft
+                +"\nTaille des fichiers restants: "+DisplaySize(leftSize)+"\n"
+            );
+            DisplayProgressBar(percent);
+        }
+
+        public void DisplayBackupRecap(string name, double transfertTime)
+        {
+            Console.WriteLine("\n\n" + 
+                "Backup : " + name + "terminé\n"
+                +"\nDurée : " + transfertTime + "ms\n"
+            );
+            DisplayProgressBar(100);
+        }
+
+        public void DisplayFiledError(string name)
+        {
+            Console.WriteLine("Échec pour le fichier " + name);
+        }
+        
+        //Verify if input is in the good range
+        private int CheckChoiceMenu(string inputUser, int minEntry, int maxEntry)
+        {
+            while(!(CheckInt(inputUser) && (Int32.Parse(inputUser) >= minEntry && Int32.Parse(inputUser)<= maxEntry)))
+            {
+                DisplayMessage(206);
+                inputUser = Console.ReadLine();
+            }
+            return Int32.Parse(inputUser);
+        }
+
+        //choose save to delete
+        public int RemovesaveChoice()
+        {
+            Console.Clear();
+            Console.WriteLine("Supprime une sauvegarde: ");
+
+            //Display all saves
+            SavesJobReport(1);
+            DisplayMessage(2);
+
+            //Display if userinput is a valid integer
+            return CheckChoiceMenu(Console.ReadLine(), 0, this.viewModel.model.saves.count);
+        }
+
+        //Choose the work to save
+        public int LaunchBackupChoice()
+        {
+            Console.Clear();
+            Console.WriteLine(
+                "Choisissez le travail à sauvegarder : " +
+                "\n\n1 - tout"
+            );
+
+            //Choose the work to save
+            SavesJobReport(2);
+            DisplayMessage(2);
+
+            //Display if input is a valid integer
+            return CheckChoiceMenu(Console.ReadLine(), 0, this.viewModel.model.saves.count + 1);
         }
     }
 }
