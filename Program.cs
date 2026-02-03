1c0848c1c4365de9336b19b591c73fd2cf1ff809
prnhpci <mjh15179389g@gmail.com>
2026-02-03 14:01:25 +0100

    fix(Program.cs): merge conflict

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
+<<<<<<< HEAD
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+=======
+using easySave_BMT.ViewModel_;
+
+namespace easySave_BMT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            ViewModel viewModel = new ViewModel();
+
+            viewModel.RunApp();
+        }
+    }
+}
+>>>>>>> 1a63296 (feat(Program.cs): created program.cs file + class)
