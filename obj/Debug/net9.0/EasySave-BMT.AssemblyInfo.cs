c07b6fab9bd3b99defeda44fcd2278734c7ec6c1
prnhpci <mjh15179389g@gmail.com>
2026-02-03 15:19:34 +0100

    style(build): dotnet run build

--- obj/Debug/net9.0/EasySave-BMT.AssemblyInfo.cs
+++ obj/Debug/net9.0/EasySave-BMT.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EasySave-BMT")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4dcff5d8c762a0d60f89d550f6edec9c3f14beb9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+021462746ee4b103b9035fdee0689dedeed3fab1")]
 [assembly: System.Reflection.AssemblyProductAttribute("EasySave-BMT")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EasySave-BMT")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
