695aff504a2b190bc5e75f6704f233ea446e7a0b
Nass213dz <nassouben44@gmail.com>
2026-02-03 09:23:25 +0100

    Class ViewModel created and constructor finished

--- ViewModel/ViewModel.cs
+++ ViewModel/ViewModel.cs
@@ -4,4 +4,17 @@ using System.Collections.Generic;  // List<list>, dictionnary etc.
 using easySave_BMT.Model_;         // Importation of "Model" directory class(es)
 using easySave_BMT.View_;          // Importation of "View" directory class(es)
 
-namespace easySave_BMT.ViewModel_; // Creation of ViewModel namespace
+namespace easySave_BMT.ViewModel_  // Creation of ViewModel namespace
+{
+    class ViewModel
+    {
+        public Model model;
+        public View view;
+
+        public ViewModel()
+        {
+            this.model = new Model();
+            this.view = new View();
+        }
+    }
+}
