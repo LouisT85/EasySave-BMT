da321bec0b395412b69fdffc71b714572f6ab65b
Nass213dz <nassouben44@gmail.com>
2026-02-03 09:10:47 +0100

    creation of viewmodel namespace

--- ViewModel/ViewModel.cs
+++ ViewModel/ViewModel.cs
@@ -1,6 +1,7 @@
-using System;                     // Library .NET
-using System.IO;                  // files/directory management
-using System.Collections.Generic; // List<list>, dictionnary etc.
-using easySave_V1.Model_;         // Importation of "Model" directory class(es)
-using easySave_V1.View_;          // Importation of "View" directory class(es)
+using System;                      // Library .NET
+using System.IO;                   // files/directory management
+using System.Collections.Generic;  // List<list>, dictionnary etc.
+using easySave_BMT.Model_;         // Importation of "Model" directory class(es)
+using easySave_BMT.View_;          // Importation of "View" directory class(es)
 
+namespace easySave_BMT.ViewModel_; // Creation of ViewModel namespace
