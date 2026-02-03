5e893a58d4fe942bd678017be43e05c47ea92482
LouisT85 <louis.tricoire@viacesi.fr>
2026-02-03 09:29:49 +0100

    creation of the constructor, the initialization of the save list and the methodes Addsav and Removesave + try/catch

--- Model/Model.cs
+++ Model/Model.cs
@@ -5,8 +5,9 @@ using System.IO;
 using easySave_V1.View_Model;
 
 namespace easySave_BMT.Model_;
+
 {
-    class Model
+    class Model 
     {
         // --- Attributes ---
         private string backupsaveSavePath = "./BackupsaveSave.json";
@@ -17,5 +18,52 @@ namespace easySave_BMT.Model_;
         {
             WriteIndented = true
         }; 
+
+        // --- Constructor ---
+        public Model()
+        {
+            // Initalize save List
+            this.saves = new List<save>();
+        }
+
+
+        // --- Methods ---
+        // Add save
+        public int Addsave(string _name, string _src, string _dst, BackupType _backupType)
+        {
+            try
+            {
+                // Add save in the program (at the end of the List)
+                this.saves.Add(new save(_name, _src, _dst, _backupType));
+                Savesaves();
+
+                // Return Success Code
+                return 101;
+            }
+            catch
+            {
+                // Return Error Code
+                return 201;
+            }
+        }
+
+        // Remove save
+        public int Removesave(int _index)
+        {
+            try
+            {
+                // Remove save from the program (at index)
+                this.saves.RemoveAt(_index);
+                Savesaves();
+
+                // Return Success Code
+                return 103;
+            }
+            catch
+            {
+                // Return Error Code
+                return 203;
+            }
+        }
     }
 }
