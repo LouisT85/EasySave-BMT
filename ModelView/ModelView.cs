2cd574523ad32a37e1937595c450fa924e516a78
Nass213dz <nassouben44@gmail.com>
2026-02-03 08:51:22 +0100

    Library/namespaces importation

---
+++ ModelView/ModelView.cs
@@ -0,0 +1,6 @@
+using System;                     // Library .NET
+using System.IO;                  // files/directory management
+using System.Collections.Generic; // List<list>, dictionnary etc.
+using easySave_V1.Model_;         // Importation of "Model" directory class(es)
+using easySave_V1.View_;          // Importation of "View" directory class(es)
+
