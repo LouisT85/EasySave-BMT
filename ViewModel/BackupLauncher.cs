5afb0d237c78d90673dc90b5229fc5592a0173db
Nassim.bms <nassouben44@gmail.com>
2026-02-12 09:16:01 +0100

    feat(viewmodel directory): add implementation test

---
+++ ViewModel/BackupLauncher.cs
@@ -0,0 +1,253 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using easySave_BMT.Model_;
+
+namespace easySave_BMT.ViewModel_.Backup
+{
+    public class BackupLauncher
+    {
+        private readonly ViewModel _viewModel;
+
+        public BackupLauncher(ViewModel viewModel)
+        {
+            _viewModel = viewModel;
+        }
+
+        public void LaunchBackupsave()
+        {
+            if (_viewModel.model.saves.Count > 0)
+            {
+                int userChoice = _viewModel.view.LaunchBackupChoice();
+
+                switch (userChoice)
+                {
+                    case 0:
+                        return;
+
+                    case 1:
+                        BackupAllSaves();
+                        break;
+
+                    default:
+                        BackupSingleSave(userChoice);
+                        break;
+                }
+                _viewModel.view.DisplayMessage(1);
+            }
+            else
+            {
+                _viewModel.view.DisplayMessage(204);
+            }
+        }
+
+        public int LaunchBackupType(Save _save)
+        {
+            DirectoryInfo dir = new DirectoryInfo(_save.src);
+
+            if (!dir.Exists && !Directory.Exists(_save.dst))
+            {
+                return 207;
+            }
+
+            var activeState = new State(0, 0, _save.src, _save.dst);
+            _save.state = activeState;
+            _viewModel.model.UpdateSaveState(_save);
+
+            return ExecuteBackupStrategy(_save, dir);
+        }
+
+        private void BackupAllSaves()
+        {
+            foreach (Save save in _viewModel.model.saves)
+            {
+                int result = LaunchBackupType(save);
+                _viewModel.view.DisplayMessage(result);
+
+                if (result == 104 || result == 105 || result == 216)
+                {
+                    _viewModel.model.FinishBackup(save);
+                }
+
+                _viewModel.view.DisplayMessage(4);
+            }
+        }
+
+        private void BackupSingleSave(int userChoice)
+        {
+            int indexsave = userChoice - 2;
+            Save selectedSave = _viewModel.model.saves[indexsave];
+            int backupResult = LaunchBackupType(selectedSave);
+            _viewModel.view.DisplayMessage(backupResult);
+
+            if (backupResult == 104 || backupResult == 105 || backupResult == 216)
+            {
+                _viewModel.model.FinishBackup(selectedSave);
+            }
+        }
+
+        private int ExecuteBackupStrategy(Save _save, DirectoryInfo _dir)
+        {
+            switch (_save.backupType)
+            {
+                case BackupType.DIFFERENTIAL:
+                    string fullBackupDir = GetFullBackupDir(_save);
+                    if (fullBackupDir != null)
+                    {
+                        return DifferentialBackupSetup(_save, _dir, fullBackupDir);
+                    }
+                    return FullBackupSetup(_save, _dir);
+
+                case BackupType.FULL:
+                    return FullBackupSetup(_save, _dir);
+
+                default:
+                    return 208;
+            }
+        }
+
+        private string GetFullBackupDir(Save _save)
+        {
+            DirectoryInfo[] dirs = new DirectoryInfo(_save.dst).GetDirectories();
+
+            foreach (DirectoryInfo directory in dirs)
+            {
+                if (directory.Name.IndexOf("_") > 0 &&
+                    _save.name == directory.Name.Substring(0, directory.Name.IndexOf("_")))
+                {
+                    return directory.FullName;
+                }
+            }
+            return null;
+        }
+
+        private int FullBackupSetup(Save _save, DirectoryInfo _dir)
+        {
+            long totalSize = 0;
+            FileInfo[] files = _dir.GetFiles("*.*", SearchOption.AllDirectories);
+
+            foreach (FileInfo file in files)
+            {
+                totalSize += file.Length;
+            }
+            return DoBackup(_save, files, totalSize);
+        }
+
+        private int DifferentialBackupSetup(Save _save, DirectoryInfo _dir, string _fullBackupDir)
+        {
+            long totalSize = 0;
+            FileInfo[] srcFiles = _dir.GetFiles("*.*", SearchOption.AllDirectories);
+            List<FileInfo> filesToCopy = new List<FileInfo>();
+
+            foreach (FileInfo file in srcFiles)
+            {
+                string currFullBackPath = _fullBackupDir + "\\" + Path.GetRelativePath(_save.src, file.FullName);
+
+                if (!File.Exists(currFullBackPath) || !IsSameFile(currFullBackPath, file.FullName))
+                {
+                    totalSize += file.Length;
+                    filesToCopy.Add(file);
+                }
+            }
+
+            if (filesToCopy.Count == 0)
+            {
+                _save.lastBackupDate = DateTime.Now.ToString("yyyy/MM/dd_HH:mm:ss");
+                _viewModel.model.AddLogInJSONFile();
+                _viewModel.view.DisplayMessage(3);
+                _viewModel.view.DisplayBackupRecap(_save.name, 0);
+                return 105;
+            }
+            return DoBackup(_save, filesToCopy.ToArray(), totalSize);
+        }
+
+        private bool IsSameFile(string path1, string path2)
+        {
+            try
+            {
+                byte[] file1 = File.ReadAllBytes(path1);
+                byte[] file2 = File.ReadAllBytes(path2);
+
+                if (file1.Length == file2.Length)
+                {
+                    for (int i = 0; i < file1.Length; i++)
+                    {
+                        if (file1[i] != file2[i])
+                        {
+                            return false;
+                        }
+                    }
+                    return true;
+                }
+                return false;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private int DoBackup(Save _save, FileInfo[] _files, long _totalSize)
+        {
+            DateTime startTime = DateTime.Now;
+            string dst = _save.dst + _save.name + "_" + startTime.ToString("yyyy-MM-dd_HH-mm-ss") + "\\";
+
+            _save.state = new State(_files.Length, _totalSize, _save.src, dst);
+            _save.lastBackupDate = startTime.ToString("yyyy/MM/dd_HH:mm:ss");
+
+            try
+            {
+                Directory.CreateDirectory(dst);
+            }
+            catch
+            {
+                return 210;
+            }
+
+            Console.Clear();
+
+            List<string> failedFiles = CopyFiles(_save, _files, _totalSize, dst);
+            DateTime endTime = DateTime.Now;
+            TimeSpan saveTime = endTime - startTime;
+            double transferTime = saveTime.TotalMilliseconds;
+
+            _viewModel.model.AddLogInJSONFile();
+            _viewModel.view.DisplayMessage(3);
+
+            foreach (string failedFile in failedFiles)
+            {
+                _viewModel.view.DisplayFiledError(failedFile);
+            }
+
+            _viewModel.view.DisplayBackupRecap(_save.name, transferTime);
+
+            return failedFiles.Count == 0 ? 104 : 216;
+        }
+
+        private List<string> CopyFiles(Save _save, FileInfo[] _files, long _totalSize, string _dst)
+        {
+            long leftSize = _totalSize;
+            int totalFile = _files.Length;
+            List<string> failedFiles = new List<string>();
+
+            for (int i = 0; i < _files.Length; i++)
+            {
+                int pourcent = ((i + 1) * 100) / totalFile;
+                long curSize = _files[i].Length;
+                leftSize -= curSize;
+
+                if (_viewModel.model.CopyFile(_save, _files[i], curSize, _dst, leftSize, totalFile, i, pourcent))
+                {
+                    Thread.Sleep((int)(curSize / 1000000));
+                    _viewModel.view.DisplayCurrentState(_save.name, totalFile - i - 1, leftSize, curSize, pourcent);
+                }
+                else
+                {
+                    failedFiles.Add(_files[i].Name);
+                }
+            }
+            return failedFiles;
+        }
+    }
+}
---
+++ ViewModel/CommandLineParser.cs
@@ -0,0 +1,109 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using easySave_BMT.ViewModel_;
+
+namespace easySave_BMT.ViewModel_.CommandLine
+{
+    public class CommandLineParser
+    {
+        public void HandleCommandLine(string[] args, CommandLineBackupRunner runner)
+        {
+            string backupArg = args[1];
+            
+            if (args.Length > 2)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("\nWarning: Only the first argument after the executable name is used.");
+                Console.WriteLine("Use semicolons (;) to separate multiple backup indices.");
+                Console.ResetColor();
+            }
+            
+            List<int> backupIndices = ParseCommandLineArguments(backupArg);
+
+            if (backupIndices != null && backupIndices.Count > 0)
+            {
+                runner.ExecuteCommandLineBackups(backupIndices);
+            }
+            else
+            {
+                ShowUsageError();
+            }
+        }
+
+        private List<int> ParseCommandLineArguments(string argument)
+        {
+            if (string.IsNullOrWhiteSpace(argument))
+                return null;
+
+            List<int> indices = new List<int>();
+
+            try
+            {
+                string[] parts = argument.Split(';');
+
+                foreach (string part in parts)
+                {
+                    if (string.IsNullOrWhiteSpace(part))
+                        continue;
+
+                    if (part.Contains("-"))
+                    {
+                        string[] range = part.Split('-');
+                        if (range.Length == 2)
+                        {
+                            int start = int.Parse(range[0].Trim());
+                            int end = int.Parse(range[1].Trim());
+
+                            if (start > 0 && end > 0 && start <= end)
+                            {
+                                for (int i = start; i <= end; i++)
+                                {
+                                    if (!indices.Contains(i))
+                                        indices.Add(i);
+                                }
+                            }
+                            else
+                            {
+                                return null;
+                            }
+                        }
+                        else
+                        {
+                            return null;
+                        }
+                    }
+                    else
+                    {
+                        int index = int.Parse(part.Trim());
+                        if (index > 0 && !indices.Contains(index))
+                            indices.Add(index);
+                        else if (index <= 0)
+                            return null;
+                    }
+                }
+
+                indices.Sort();
+                return indices;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        private void ShowUsageError()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\nInvalid command line argument format.");
+            Console.WriteLine("Usage examples:");
+            Console.WriteLine("  EasySave.exe 1;3;5       (execute backups 1, 3 and 5)");
+            Console.WriteLine("  EasySave.exe 1-3;5       (execute backups 1, 2, 3 and 5)");
+            Console.WriteLine("  EasySave.exe 1;2-4;7     (execute backups 1, 2, 3, 4 and 7)");
+            Console.WriteLine("  EasySave.exe 1-5         (execute backups 1 to 5)");
+            Console.ResetColor();
+            Console.WriteLine("\nPress Enter to continue to interactive menu...");
+            Console.ReadLine();
+        }
+    }
+}
